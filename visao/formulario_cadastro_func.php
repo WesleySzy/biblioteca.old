a56dcdb7498078816b1f5b23327df980f494d758
WesleySzy <42793329+WesleySzy@users.noreply.github.com>
2019-05-26 13:41:02 -0300

    Initial commit

---
+++ visao/formulario_cadastro_func.php
@@ -0,0 +1,38 @@
+<html>
+<head>
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<title>Cadastro de Alunos</title>
+		<link rel="stylesheet" href="css/bootstrap.css" />
+		<link rel="stylesheet" href="css/style.css" />
+		<link rel="stylesheet" href="css/normalize.css" />
+		<style type="text/css" media="screen">
+			body {
+				background: #fff;
+			}
+		</style>
+	</head>
+
+<body>
+<fieldset>
+	<center><h1>Cadastrar Funcionario</h1></center>
+<center><form name="signup" method="post" action="cadastrando_funcionario.php">
+	<div class="control-group">
+<label class="control-label"> Nome Int: </label><input type="text" placeholder="Type something…" name="nome_funcionario" />
+	<div class="controls">
+<br />
+<div class="control-group">
+<label class="control-label"> Endereco: </label><input type="text" placeholder="Type something…" name="endereco_funcionario" /><br />
+<div class="controls">
+<br />
+<div class="control-group">
+<label class="control-label"> Telefone: </label><input type="text" placeholder="Type something…" name="telefone_funcionario" /><br />
+<div class="controls">
+<br />
+
+<input type="submit" value="Cadastrar">
+<input type="reset" value="Novo Cadastro">
+<a href="javascript:history.back()">Voltar</a>
+ </fieldset>
+</form></center>
+</body>
+</html>
